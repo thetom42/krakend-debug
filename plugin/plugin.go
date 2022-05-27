251920fd47531dd04565c4c777fe2ca78a8ae4f3
Thomas Hessel <thetom42@gmail.com>
2022-05-27 07:16:51 +0000

    Expand plugin and start with docker-compose

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"strconv"
+	"strings"
+	"time"
 )
+
+const plugin_name = "krakend-debugger"
 
 var (
 	logger Logger = nil
@@ -77,10 +86,94 @@ func (r requestWrapper) Headers() map[string][]string { return r.headers }
 
 type registerer string
 
+// ClientRegisterer is the symbol the plugin loader will try to load. It must implement the RegisterClient interface
+var ClientRegisterer = registerer(plugin_name)
+
+func (r registerer) RegisterClients(f func(name string, handler func(context.Context, map[string]interface{}) (http.Handler, error))) {
+	f(string(r), r.registerClients)
+}
+
+func (r registerer) registerClients(ctx context.Context, extra map[string]interface{}) (h http.Handler, e error) {
+	// check the passed configuration and initialize the plugin
+	name, ok := extra["name"].(string)
+	if !ok {
+		return nil, errors.New("wrong config")
+	}
+	if name != string(r) {
+		return nil, fmt.Errorf("unknown register %s", name)
+	}
+	// return the actual handler wrapping or your custom logic so it can be used as a replacement for the default http client
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		makeOriginalRequest(w, req)
+	}), nil
+}
+
+// HandlerRegisterer is the symbol the plugin loader will try to load. It must implement the Registerer interface
+var HandlerRegisterer = registerer(plugin_name)
+
+func (r registerer) RegisterHandlers(f func(name string, handler func(context.Context, map[string]interface{}, http.Handler) (http.Handler, error))) {
+	f(string(r), r.registerHandlers)
+}
+
+func (r registerer) registerHandlers(ctx context.Context, extra map[string]interface{}, old http.Handler) (h http.Handler, e error) {
+	// check the passed configuration and initialize the plugin
+	name, ok := extra["name"].(string)
+	if !ok {
+		return nil, errors.New("wrong config")
+	}
+	if name != string(r) {
+		return nil, fmt.Errorf("unknown register %s", name)
+	}
+
+	// return the actual handler wrapping or your custom logic so it can be used as a replacement for the default http handler
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		// Create a unique request id - "github.com/google/uuid.NewString()" causes "plugin was built with a different version of package"
+		t := time.Now().UnixNano()
+		reqid := strconv.FormatInt(t, 36) + "_" + strconv.Itoa(rand.Intn(32768))
+		req.Header["Requuid"] = []string{reqid}
+
+		dump, err := httputil.DumpRequest(req, true)
+		if err != nil {
+			logF(err)
+		}
+		rqstr := string(dump)
+		logD("req", rqstr)
+
+		if true {
+			// Request id and URL are the main keys
+			keys := map[string]string{"id": reqid, "url": req.URL.Path}
+
+			if false {
+				// Add first 6 path segments, i.e. URL parts delimited by '/' as additional keys
+				// E.g. URL /HardwarePricesDataManagement/v2/HardwarePrices/HardwarePrices
+				// will fill seg0 = "", // Usually empty
+				//           seg1 = "HardwarePricesDataManagement",
+				//           seg2 ="v2",
+				//           seg3 = "HardwarePrices",
+				//           seg4 = "HardwarePrices"
+				km := []string{"seg0", "seg1", "seg2", "seg3", "seg4", "seg5", "segremain"}
+				segs := strings.SplitN(req.URL.Path, "/", len(km))
+				for i, s := range segs {
+					keys[km[i]] = s
+				}
+			}
+
+			// Get non-blocking write client
+			p := influx.NewPoint("payload",
+				keys,
+				map[string]interface{}{"request": rqstr},
+				time.Now())
+			writeAPI.WritePoint(p)
+		}
+
+		old.ServeHTTP(w, req)
+	}), nil
+}
+
 // ModifierRegisterer is the symbol the plugin loader will be looking for. It must
 // implement the plugin.Registerer interface
 // https://github.com/luraproject/lura/blob/master/proxy/plugin/modifier.go#L71
-var ModifierRegisterer = registerer("krakend-debugger")
+var ModifierRegisterer = registerer(plugin_name)
 
 // RegisterModifiers is the function the plugin loader will call to register the
 // modifier(s) contained in the plugin using the function passed as argument.
@@ -166,6 +259,62 @@ func (r registerer) responseModifierFactory(
 		logD(fmt.Sprintf("[PLUGIN: %s] status code: %v", ModifierRegisterer, resp.StatusCode()))
 
 		return input, nil
+	}
+}
+
+func makeOriginalRequest(w http.ResponseWriter, req *http.Request) {
+	// Depending on the HTTP client software, HTTP protocol version, and
+	// any intermediaries between the client and the Go server, it may not
+	// be possible to read from the Request.Body after writing to the
+	// ResponseWriter. Cautious handlers should read the Request.Body
+	// first, and then reply.
+
+	// Dump incoming request
+	dumprq, rqerr := httputil.DumpRequest(req, true)
+
+	client := &http.Client{}
+	// Send an HTTP request and returns an HTTP response object.
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		// return
+	}
+	defer resp.Body.Close()
+
+	// headers
+	for name, values := range resp.Header {
+		w.Header()[name] = values
+	}
+
+	// status (must come after setting headers and before copying body)
+	w.WriteHeader(resp.StatusCode)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	w.Write(body)
+
+	if rqerr != nil {
+		logF("rqerr", rqerr.Error())
+	}
+	rqstr := string(dumprq)
+	logD("backend req", rqstr)
+
+	dumprs, rserr := httputil.DumpResponse(resp, false)
+	if rserr != nil {
+		logF("rserr", rserr.Error())
+	}
+	var reqid string
+	reqid = req.Header["Requuid"][0]
+	res := string(dumprs) + string(body)
+	logD("req_id ", reqid)
+	logD("res ", res, "\n")
+
+	if true {
+		// get non-blocking write client
+		p := influx.NewPoint("payload",
+			map[string]string{"id": reqid},
+			map[string]interface{}{"backend_request": rqstr, "response": res},
+			time.Now())
+		writeAPI.WritePoint(p)
 	}
 }
 
